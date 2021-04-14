18a4b0f3711d2418a61a60cc15350940287ed03e
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-04-14 19:20:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const nav = document.querySelector('.header__nav');
+const menu = document.querySelector('#menu');
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('header__nav--active');
+});
