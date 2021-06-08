035dcbcb751a1bd6a20372549c5181fd718dffe5
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-06-08 15:19:05 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const nav = document.querySelector('.header__nav');
-const menu = document.querySelector('#menu');
-
-menu.addEventListener('click', () => {
-  nav.classList.toggle('header__nav--active');
-});
